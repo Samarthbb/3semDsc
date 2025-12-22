341a969febe2a6dbada20ccd97710d68cb3d95ac
Samarthbb <gaganbeleamgi@gmail.com>
2025-12-22 23:46:17 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,59 +1,100 @@
-// client.c
 #include <stdio.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <stdlib.h>
+typedef struct BST {
+ int data;
+ struct BST *lchild, *rchild;
+} node;
+node *get_node();
+void insert(node *root, node *new_node);
+void inorder(node *temp);
+void preorder(node *temp);
+void postorder(node *temp);
 int main() {
-    int soc = socket(PF_INET, SOCK_STREAM, 0);
- if (soc < 0) return 1; 
- struct sockaddr_in addr = {
- .sin_family = AF_INET,
- .sin_port = htons(7891),
- .sin_addr.s_addr = inet_addr("127.0.0.1")
- };
- if (connect(soc, (struct sockaddr *) &addr, sizeof(addr)) == 0) {
- printf("Connected to server\nEnter file name: ");
- char fname[50], buffer[1024];
- scanf("%s", fname);
-send(soc, fname, sizeof(fname), 0);
- printf("Response:\n");
-while (recv(soc, buffer, sizeof(buffer), 0) > 0)
- printf("%s", buffer);
- } else {
- perror("Connection failed");
+ int choice;
+ int ans = 1;
+ node *new_node, *root = NULL;
+ printf("\nProgram For Binary Search Tree");
+ do {
+ printf("\n\n1. Create");
+ printf("\n2. Recursive Traversals");
+ printf("\n3. Exit");
+ printf("\nEnter your choice: ");
+ scanf("%d", &choice);
+ switch(choice) {
+ case 1:
+ do {
+ new_node = get_node();
+ printf("Enter the element: ");
+ scanf("%d", &new_node->data);
+ if (root == NULL) // Tree is not created yet
+ root = new_node;
+ else
+ insert(root, new_node);
+ printf("Want to enter more elements? (1/0): ");
+ scanf("%d", &ans);
+ } while(ans == 1);
+ break;
+ case 2:
+ if (root == NULL)
+ printf("Tree is not created yet.\n");
+ else {
+ printf("\nInorder traversal: ");
+ inorder(root);
+ printf("\nPreorder traversal: ");
+ preorder(root);
+ printf("\nPostorder traversal: ");
+ postorder(root);
+ printf("\n");
  }
-close(soc);
+ break;
+ case 3:
+ printf("Exiting program.\n");
+ exit(0);
+ default:
+ printf("Invalid choice, please try again.\n");
+ }
+ } while (1);
  return 0;
 }
-
-//server.c
-#include <stdio.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-int main() {
-int welcome = socket(PF_INET, SOCK_STREAM, 0);
-struct sockaddr_in addr = { .sin_family = AF_INET, .sin_port = htons(7891),
-.sin_addr.s_addr = inet_addr("127.0.0.1") };
- bind(welcome, (struct sockaddr*)&addr, sizeof(addr));
- printf("Server is online, waiting for connections...\n");
- listen(welcome, 5);
-nt new_soc = accept(welcome, NULL, NULL);
- char fname[50], buffer
- recv(new_soc, fname, sizeof(fname), 0);
- printf("Request received for file: %s\n", fname);
-int fd = open(fname, O_RDONLY);
- if (fd < 0) {
- send(new_soc, "File not found\n", 15, 0);
- printf("File not found, notified client.\n");
- } else {
- printf("File found, sending contents to client...\n");
- int n;
- while ((n = read(fd, buffer, sizeof(buffer))) > 0)
- send(new_soc, buffer, n, 0);
+node *get_node() {
+ node *temp = (node *) malloc(sizeof(node));
+ temp->lchild = NULL;
+ temp->rchild = NULL;
+ return temp;
+}
+void insert(node *root, node *new_node) {
+ if (new_node->data < root->data) {
+ if (root->lchild == NULL)
+ root->lchild = new_node;
+ else
+ insert(root->lchild, new_node);
  }
- close(fd);
- close(new_soc);
- close(welcome);
- printf("Request completed.\n");
- return 0;
+ else if (new_node->data > root->data) {
+ if (root->rchild == NULL)
+ root->rchild = new_node;
+ else
+ insert(root->rchild, new_node);
+ }
+ // If equal, do nothing or handle duplicates as per your need
+}
+void inorder(node *temp) {
+ if (temp != NULL) {
+ inorder(temp->lchild);
+ printf("%d ", temp->data);
+ inorder(temp->rchild);
+ }
+}
+void preorder(node *temp) {
+ if (temp != NULL) {
+ printf("%d ", temp->data);
+ preorder(temp->lchild);
+ preorder(temp->rchild);
+ }
+}
+void postorder(node *temp) {
+ if (temp != NULL) {
+ postorder(temp->lchild);
+ postorder(temp->rchild);
+ printf("%d ", temp->data);
+ }
 }
